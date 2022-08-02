940d38a78647d8c36ca41a2b2bb724aa70b828cf
Mauricio Robayo <rfmaj@gmail.com>
2022-08-01 20:25:01 -0500

    Initial commit

---
+++ schemas/something.js
@@ -0,0 +1,12 @@
+export default {
+  name: "something",
+  title: "Something",
+  type: "object",
+  fields: [
+    {
+      name: "foo",
+      title: "bar",
+      type: "string",
+    },
+  ],
+};
---
+++ schemas/test.js
@@ -0,0 +1,18 @@
+export default {
+  name: "test",
+  title: "Test",
+  type: "document",
+  fields: [
+    {
+      name: "something1",
+      type: "something",
+      group: "group",
+    },
+  ],
+  groups: [
+    {
+      name: "group",
+      title: "Group1",
+    },
+  ],
+};
